e70dfc17030a0b56d0671756b969ff63709b0729
Samuel <samuelosoriotamayo@gmail.com>
2023-10-06 17:13:20 -0500

    Delete

--- Js/CRUD.js
+++ Js/CRUD.js
@@ -62,5 +62,10 @@ export class CRUD {
         return true;
     }
 
-   
+    delete(id) {
+        this.#checkThatElementExistsWithId(id);
+        this.#data.splice(id,1);
+        this.#save();
+        return true;
+    }
 }
